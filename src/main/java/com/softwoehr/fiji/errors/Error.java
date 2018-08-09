138abcd73d9cca678aaf288d1ef822056541e77d
Blue Kelp <github@bluekelp.com>
2018-08-09 13:37:58 -0700

    reformat/cleanup errors

--- src/main/java/com/softwoehr/fiji/errors/Error.java
+++ src/main/java/com/softwoehr/fiji/errors/Error.java
@@ -28,239 +28,141 @@
 
 package com.softwoehr.fiji.errors;
 
-/** An Error class for SoftWoehr
+/**
+ * An Error class for SoftWoehr
  *
  * @author $Author: jwoehr $
  * @version $Revision: 1.1.1.1 $
  */
 public class Error extends java.lang.Error {
-    /** Errors pertaining to com.SoftWoehr.desktop. */
     public static class desktop {
-        /** Errors pertaining to com.SoftWoehr.desktop.shell. */
         public static class shell {
 
-            /** Invalid numeric conversion base for Interpreter. */
+            // Invalid numeric conversion base for Interpreter.
             public static class BadBase extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public BadBase(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public BadBase(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                          /* BadBase*/
+            }
 
-            /** Invalid character display code page requested from host system.*/
+            // Invalid character display code page requested from host system.
             public static class BadEncoding extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public BadEncoding(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public BadEncoding(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* BadEncoding*/
+            }
 
-            /** Invalid name offered for a Semantic.*/
+            // Invalid name offered for a Semantic.
             public static class BadName extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public BadName(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public BadName(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* BadEncoding*/
+            }
 
-            /** Attempt to perform a Variable operation on a non-Variable.*/
+            // Attempt to perform a Variable operation on a non-Variable.
             public static class NonVariable extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public NonVariable(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public NonVariable(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* NonVariable*/
+            }
 
-            /** Attempt to perform a Value operation on a non-Value.*/
+            // Attempt to perform a Value operation on a non-Value.
             public static class NonValue extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public NonValue(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public NonValue(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                         /* NonValue*/
+            }
 
-            /** Exception encoutered in execution semantics of a Primitive.*/
+            // Exception encoutered in execution semantics of a Primitive.
             public static class BadPrimitiveExecute extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public BadPrimitiveExecute(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public BadPrimitiveExecute(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                              /* BadPrimitiveExecute*/
+            }
 
-            /** Exception encoutered in compilation semantics of a Primitive.*/
+            // Exception encoutered in compilation semantics of a Primitive.
             public static class BadPrimitiveCompile extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public BadPrimitiveCompile(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public BadPrimitiveCompile(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                              /* BadPrimitiveCompile*/
+            }
 
-            /** Exception encoutered in compilation semantics of a Primitive.*/
+            // Exception encoutered in compilation semantics of a Primitive.
             public static class CompileToValue extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public CompileToValue(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public CompileToValue(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                   /* CompileToValue*/
+            }
 
-            /** Name not found in the Interpreter search order.*/
+            // Name not found in the Interpreter search order.
             public static class NameNotFound extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public NameNotFound(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public NameNotFound(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                     /* NameNotFound*/
+            }
 
-            /** Control flow stack imbalance.*/
+            // Control flow stack imbalance.
             public static class ControlFlowStackImbalance extends com.softwoehr.fiji.errors.Error {
-
-                /** Arity/1 Throwable constructor.
-                 * @param t  */
                 public ControlFlowStackImbalance(Throwable t) {
                     super(t);
                 }
-
-                /** Arity/2 Throwable constructor.
-                 * @param s
-                 * @param t  */
                 public ControlFlowStackImbalance(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                     /* ControlFlowStackImbalance*/
-        }                                                              /* shell*/
-    }                                                              /* desktop*/
+            }
+        }
+    }
 
-    /** A buncha error types. */
     public static class BackToMain extends com.softwoehr.fiji.errors.Error {
-
-        /** Arity/1 Throwable constructor.
-         * @param t  */
         public BackToMain(Throwable t) {
             super(t);
         }
-
-        /** Arity/2 Throwable constructor.
-         * @param s
-         * @param t  */
         public BackToMain(String s, java.lang.Throwable t) {
-            super(s,t);
+            super(s, t);
         }
-
     }
 
-    /** A buncha error types. */
     public static class BadArgToMain extends com.softwoehr.fiji.errors.Error {
-
-        /** Arity/1 Throwable constructor.
-         * @param t  */
         public BadArgToMain(Throwable t) {
             super(t);
         }
-
-        /** Arity/2 Throwable constructor.
-         * @param s
-         * @param t  */
         public BadArgToMain(String s, java.lang.Throwable t) {
-            super(s,t);
+            super(s, t);
         }
     }
 
-    /** Arity/0 ctor. */
-    private Error() {}
-
-    /** Arity/1 Throwable constructor.
-     * @param t  */
-    public Error(Throwable t) {
+    Error(Throwable t) {
         super(t);
     }
 
-    /** Arity/2 Throwable constructor.
-     * @param s
-     * @param t  */
-    public Error(String s, java.lang.Throwable t) {
+    Error(String s, java.lang.Throwable t) {
         super(s, t);
     }
-}                                                     /* End of Error class*/
-
-/*  End of Error.java */
+}
--- src/main/java/com/softwoehr/fiji/errors/Exceptions.java
+++ src/main/java/com/softwoehr/fiji/errors/Exceptions.java
@@ -28,326 +28,296 @@
 
 package com.softwoehr.fiji.errors;
 
-/** An Exception base class for SoftWoehr.
+
+/*  End of Exceptions.java */
+
+/**
+ * An Exception base class for SoftWoehr.
  *
  * @author $Author: jwoehr $
  * @version $Revision: 1.1.1.1 $
  */
 public class Exceptions extends java.lang.Exception {
-    /** Exceptions pertaining to com.SoftWoehr.desktop. */
     public static class desktop {
-        /** Exceptions pertaining to com.SoftWoehr.desktop.shell. */
         public static class shell {
 
-            /** Stack too shallow for operation. */
+            /**
+             * Stack too shallow for operation.
+             */
             public static class StackUnderflow extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public StackUnderflow(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public StackUnderflow(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                   /* StackUnderflow*/
+            }
 
-            /** An entity was passed as an instance of a class but is not. */
+            /**
+             * An entity was passed as an instance of a class but is not.
+             */
             public static class NotClassInstance extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NotClassInstance(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NotClassInstance(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                 /* NotClassInstance*/
+            }
 
-            /** An entity was passed as an instance of a reflected class but is not. */
+            /**
+             * An entity was passed as an instance of a reflected class but is not.
+             */
             public static class NonReflectedType extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NonReflectedType(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NonReflectedType(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                 /* NonReflectedType*/
+            }
 
-            /** A String was passed to represent a class name but doesn't. */
+            /**
+             * A String was passed to represent a class name but doesn't.
+             */
             public static class NotClassName extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NotClassName(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NotClassName(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                     /* NotClassName*/
+            }
 
-            /** Invalid numeric conversion base for Interpreter. */
+            /**
+             * Invalid numeric conversion base for Interpreter.
+             */
             public static class BadBase extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadBase(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadBase(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                          /* BadBase*/
+            }
 
-            /** Invalid character display code page requested from host system.*/
+            /**
+             * Invalid character display code page requested from host system.
+             */
             public static class BadEncoding extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadEncoding(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadEncoding(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* BadEncoding*/
+            }
 
-            /** Invalid name offered for a Semantic.*/
+            /**
+             * Invalid name offered for a Semantic.
+             */
             public static class BadName extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadName(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadName(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* BadEncoding*/
+            }
 
-            /** Attempt to perform a Variable operation on a non-Variable.*/
+            /**
+             * Attempt to perform a Variable operation on a non-Variable.
+             */
             public static class NonVariable extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NonVariable(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NonVariable(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* NonVariable*/
+            }
 
-            /** Attempt to perform a Value operation on a non-Value.*/
+            /**
+             * Attempt to perform a Value operation on a non-Value.
+             */
             public static class NonValue extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NonValue(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NonValue(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                         /* NonValue*/
+            }
 
-            /** Exception encoutered in execution semantics of a Primitive.*/
+            /**
+             * Exception encoutered in execution semantics of a Primitive.
+             */
             public static class BadPrimitiveExecute extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadPrimitiveExecute(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadPrimitiveExecute(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                              /* BadPrimitiveExecute*/
+            }
 
-            /** Exception encoutered in execution semantics of a Definition.*/
+            /**
+             * Exception encoutered in execution semantics of a Definition.
+             */
             public static class BadDefinitionExecute extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadDefinitionExecute(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadDefinitionExecute(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                             /* BadDefinitionExecute*/
+            }
 
-            /** Exception encoutered in compilation semantics of a Primitive.*/
+            /**
+             * Exception encoutered in compilation semantics of a Primitive.
+             */
             public static class BadPrimitiveCompile extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadPrimitiveCompile(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadPrimitiveCompile(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                              /* BadPrimitiveCompile*/
+            }
 
-            /** Exception encoutered in compilation semantics of a Definition.*/
+            /**
+             * Exception encoutered in compilation semantics of a Definition.
+             */
             public static class BadDefinitionCompile extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BadDefinitionCompile(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BadDefinitionCompile(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                             /* BadDefinitionCompile*/
+            }
 
-            /** Exception encoutered in compilation semantics of a Value.*/
+            /**
+             * Exception encoutered in compilation semantics of a Value.
+             */
             public static class CompileToValue extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public CompileToValue(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public CompileToValue(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                   /* CompileToValue*/
+            }
 
-            /** Name not found in the Interpreter search order.*/
+            /**
+             * Name not found in the Interpreter search order.
+             */
             public static class NameNotFound extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public NameNotFound(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public NameNotFound(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                     /* NameNotFound*/
+            }
 
-            /** Control flow stack imbalance.*/
+            /**
+             * Control flow stack imbalance.
+             */
             public static class ControlFlowStackImbalance extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public ControlFlowStackImbalance(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public ControlFlowStackImbalance(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                        /* ControlFlowStackImbalance*/
+            }
 
-            /** ParamaterizedPrimitive possess useless parameter object.*/
+            /**
+             * ParamaterizedPrimitive possess useless parameter object.
+             */
             public static class InvalidParameterObject extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public InvalidParameterObject(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public InvalidParameterObject(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                           /* InvalidParameterObject*/
+            }
 
-            /** Non-Boolean passed to a conditional. */
+            /**
+             * Non-Boolean passed to a conditional.
+             */
             public static class ConditionalNonBoolean extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public ConditionalNonBoolean(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public ConditionalNonBoolean(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                            /* ConditionalNonBoolean*/
+            }
 
-            /** Problem during branch resolution, bad branch, bad object types, etc. */
+            /**
+             * Problem during branch resolution, bad branch, bad object types, etc.
+             */
             public static class BranchResolution extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public BranchResolution(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public BranchResolution(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                 /* BranchResolution*/
+            }
 
-            /** Interpret-time use of compile-only syntax. */
+            /**
+             * Interpret-time use of compile-only syntax.
+             */
             public static class CompileOnly extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public CompileOnly(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public CompileOnly(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                      /* CompileOnly*/
+            }
 
-            /** Problem compiling an if-branch. */
             public static class OpenIfBranch extends com.softwoehr.fiji.errors.Exceptions {
-
-                /** Arity/1 Throwable constructor. */
                 public OpenIfBranch(Throwable t) {
                     super(t);
                 }
 
-                /** Arity/2 Throwable constructor. */
                 public OpenIfBranch(String s, java.lang.Throwable t) {
-                    super(s,t);
+                    super(s, t);
                 }
-            }                                                     /* OpenIfBranch*/
-        }                                                              /* shell*/
-    }                                                              /* desktop*/
+            }
+        }
+    }
 
-    /** Arity/0 ctor. */
-    private Exceptions() {}
-
-    /** Arity/1 Throwable constructor. */
-    public Exceptions(Throwable t) {
+    Exceptions(Throwable t) {
         super(t);
     }
 
-    /** Arity/2 Throwable constructor. */
-    public Exceptions(String s, java.lang.Throwable t) {
+    Exceptions(String s, java.lang.Throwable t) {
         super(s, t);
     }
-
-}                                                     /* End of Exceptions class*/
-
-/*  End of Exceptions.java */
+}
