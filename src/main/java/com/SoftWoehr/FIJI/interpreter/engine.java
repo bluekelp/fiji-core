ded7fd340693ab2b20a375cf486751788b249d62
Blue Kelp <github@bluekelp.com>
2018-08-08 17:30:58 -0700

    extract JavaArgs to own file

--- src/main/java/com/SoftWoehr/FIJI/interpreter/engine.java
+++ src/main/java/com/SoftWoehr/FIJI/interpreter/engine.java
@@ -33,6 +33,7 @@ import java.io.*;
 import java.util.*;
 import java.lang.reflect.*;
 
+import com.SoftWoehr.FIJI.JavaArgs;
 import com.SoftWoehr.SoftWoehr;
 import com.SoftWoehr.util.*;
 
@@ -2361,49 +2362,4 @@ public class engine implements SoftWoehr, verbose {
     
 }                                                    /* End of engine class*/
 
-/** A class to represent arguments passed to Java method resolution,
- * and thence to a Java method invocation. Necessitated by the fact
- * that arguments sometimes must be of one class but cast as another,
- * e.g., java.lang.String.equals(java.lang.Object o) but the Object
- * is a String.
- */
-class JavaArgs extends Vector {
-    /** Create a JavaArgs object to pass args to java method invocation.
-     */
-    public JavaArgs() {}                     /* Needs an arity/1 on Object[]*/
-    
-    /** Return an object array of the elements of the vector.
-     * All the objects in the JavaArgs vector are stored in JavaParam's,
-     * along with their nominal class for the purpose of method signature
-     * resolution.
-     * @see JavaParam#
-     * @return An array of objects representing the arguments stored
-     * in a JavaArgs
-     */
-    public synchronized Object[] toObjectArray() {
-        int sz = size();
-        Object result[] = new Object[sz];
-        Enumeration e = elements();
-        for (int i = 0; i < sz ; ++i) {
-            result[i] = ((JavaParam) (e.nextElement())).getRealObject();
-        }                                                          /* End for*/
-        return result;
-    }
-    
-    /** Return a Class array of the classes of the elements of the vector.
-     * All the objects and their nominal classes for the purpose of
-     * method signature resolution are stored as JavaParams in the vector.
-     * see com.SoftWoehr.desktop.shell.JavaParam#
-     * @return The array of classes from the vector.
-     */
-    public synchronized Class[] toClassArray() {
-        int sz = size();
-        Class result[] = new Class[sz];
-        Enumeration e = elements();
-        for (int i = 0; i < sz ; ++i) {
-            result[i] = ((JavaParam) (e.nextElement())).getSignatureClass();
-        }                                                          /* End for*/
-        return result;
-    }
-}                                                /* End of JavaArgs class*/
 /*  End of engine.java */
